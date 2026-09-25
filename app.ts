cee6bd874e6d5a7f8d3ecec7983aefa1394d2f17
Chaitanya Gomaskar <chaitanyagomaskar25@navgurukul.org>
2026-09-25 11:41:55 +0530

    first commit

---
+++ app.ts
@@ -0,0 +1,49 @@
+// let abc: string = "hello chaitanya gomaskar"
+// console.log(abc)
+
+// let n = [10,20,30,"djd"]
+// console.log(n)
+
+// let marks: number[] = [10,20,20]
+// console.log(marks)
+
+// let user : Array<string> = ["mohi", "rohit"]
+// console.log(user)
+
+// let number : Array<number> = [1,2,3,3]
+// console.log(number)
+
+// let a : [string, number] = ["addh", 344]
+// console.log(a)
+
+// function add(a:number, b:number) {
+//     console.log(a+b)
+// }
+
+// add(12,2)
+
+// =============================
+
+// function add(a:number, b:number):number {
+//     return a+b
+// }
+
+// console.log(add(12,2))
+
+// ===================
+// function greet(age?:number,username:string="Guest") {
+//     console.log(age,username)
+// }
+
+// greet(12,"chaitanya")Search
+
+// greet()
+
+// =============================
+// const addNumber = (a:number, b:number):number=>{
+// return a+b
+// }z
+// console.log(addNumber(1,2))
+
+// =======================================
+ 
